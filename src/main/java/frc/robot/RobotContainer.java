2c08d8fd1f5d8cd56b092817607ebff9f14c8936
feds2 <zayn.baig@gmail.com>
2024-01-20 15:39:29 -0500

    Intake functions / commands for rotating intake wheels, and rotating arm, and encoders

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,22 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.Intake.IntakeIn;
+import frc.robot.subsystems.Intake.Intake;
 
 public class RobotContainer {
+  private final Intake intake;
+  private final CommandXboxController driverController;
   public RobotContainer() {
+    intake = new Intake();
+    driverController = new CommandXboxController(0);
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    driverController.b().whileTrue(new IntakeIn(intake, 0.1));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
