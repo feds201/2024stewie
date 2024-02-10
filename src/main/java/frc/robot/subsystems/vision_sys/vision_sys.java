e9de7668d948b4d18aad507cd1b372909eb57785
RITESH RAJ AS <riteshrajas.arulselvan@gmail.com>
2024-02-10 18:16:58 -0500

    Remove unused code and refactor VisionSubsystem

---
+++ src/main/java/frc/robot/subsystems/vision_sys/vision_sys.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.vision_sys;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public abstract class vision_sys extends SubsystemBase {
+  /** Creates a new vision_sys. */
+  public vision_sys() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
