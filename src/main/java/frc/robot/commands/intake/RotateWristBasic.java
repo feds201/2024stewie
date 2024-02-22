e1f6454709038b3f4dd9fc3c5e6758e47c54abbe
imzaynb <zayn.baig@gmail.com>
2024-02-22 17:46:49 -0500

    Fixed folder names ughhh

--- src/main/java/frc/robot/commands/intake/RotateWristBasic.java
+++ src/main/java/frc/robot/commands/intake/RotateWristBasic.java
@@ -1,7 +1,7 @@
-package frc.robot.commands.Intake;
+package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Intake.Intake;
+import frc.robot.subsystems.intake.Intake;
 
 public class RotateWristBasic extends Command {
 
