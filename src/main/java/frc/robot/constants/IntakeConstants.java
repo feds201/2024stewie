0c7158ae029e0dceb3adf837ffe45a0fd3b4684f
feds2 <zayn.baig@gmail.com>
2024-03-30 13:56:25 -0400

    hello

--- src/main/java/frc/robot/constants/IntakeConstants.java
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -4,14 +4,14 @@ import edu.wpi.first.math.controller.PIDController;
 
 public class IntakeConstants {
   public static class WristPID {
-    public final static double kP = 0.002;
+    public final static double kP = 0.001;//0.002
     public final static double kI = 0.001;
     public final static double kD = 0.0;
     public final static double kIZone = Double.POSITIVE_INFINITY;
     public final static double kRotationTolerance = 10;
     public static final double kSpitOutPosition = 201;
-    public final static double kWristNotePosition = 302;
-    public final static double kWristIdlePosition = 92;
+    public final static double kWristNotePosition = 308;
+    public final static double kWristIdlePosition = 88;
     public  final static  double kAmpPosition = 223;
     public final static double kWristShooterFeederSetpoint = 94; // TODO: Ideally all of the above positions should be based on this "home" position so we only have to change this
 
